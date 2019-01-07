8245ba13acdd78b4a17d21e93a9ce8d1d327a275
umezawa.kanta <kanta0930@gmail.com>
2019-01-08 08:37:02 +0900

    add volumeOfSphere

---
+++ volumeSphere.php
@@ -0,0 +1,11 @@
+#! /usr/bin/php
+
+<?php
+    $radius=48;
+    $pie=3.14285714286;
+    $volume=(4/3)*$pie*($radius*$radius*$radius);
+    echo "Volume of the Sphere=";
+    echo $volume;
+    echo "\n";
+?>
+
